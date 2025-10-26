355a817478148afb62796691ac0cd176f12c12f2
Harsha Jain <harshajain6114@gmail.com>
2025-10-26 14:11:24 +0530

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,35 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+      }
+    }
+    
+    // Fix for ethers/wagmi
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    
+    return config
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
